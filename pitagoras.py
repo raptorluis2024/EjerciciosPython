65255544d436e3157c161e78155d57297a2bb33f
Christian Sarmiento <christian.sarmiento@gmail.com>
2024-07-25 20:33:17 -0400

    mod pitagora

--- pitagoras.py
+++ pitagoras.py
@@ -4,3 +4,17 @@ es igual a la suma del cuadrado de los catetos (lados).
 c^2 = a^2 + b^2
 Elaborar un programa que lea el tamaño de los lados A y B, y calcule e imprima C (hipotenusa)
 '''
+
+import math
+
+catA = float(input("Ingrese medida cateto A: "))
+catB = float(input("Ingrese medida cateto B: "))
+
+hipotenusa = math.pow(catA,2) + math.pow(catB,2)
+hipotenusa = round(math.sqrt(hipotenusa),2)
+
+#hipotenusa =  math.sqrt(math.pow(catA,2) + math.pow(catB,2))
+print(f"""La hipotenusa para un triangulo de cateto {catA}
+y cateto {catB} = {hipotenusa}
+      """)
+
