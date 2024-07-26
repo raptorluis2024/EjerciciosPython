60366ef1c771e7ba62a80cab733a601f42633a02
Luis Pena Herrera <raptor_luis@hotmail.com>
2024-07-26 16:52:02 -0400

    add ejercicios 25-07

---
+++ 26-07-2024/ejercicio1.py
@@ -0,0 +1,18 @@
+""""
+Crear un algoritmo que permita ingresar los siguientes datos:
+    cantidad de productos
+    precio unitario
+    departamento (1. Linea blanca, 2.Aseo y Limpieza 3.Refrigeración)
+Si la cantidad de productos es mayor a 10 y corresponde al departamento de aseo y limpieza,
+el total a pagar tiene un descuento del 8%
+
+Cuando el producto es de refrigeración se debe agregar un 1% de recargo por concepto de 
+traslado
+
+Los productos de línea blanca están con un 20% de descuento pero es máximo 2 unidades por 
+persona.
+
+Al finalizar debe mostrar el detalle de los datos ingresados, los descuentos y/o recargos
+correspondientes y el total a pagar por el cliente
+    
+"""
---
+++ 26-07-2024/ejercicio2.py
@@ -0,0 +1,11 @@
+"""
+Una panadería vende (H)allullas, (M)arraquetas, pan (P)ita e (I)ntegral
+El kilo de cada uno es 1990, 2010 y 2500 respectivamente
+El programa debe solicitar al usuario la cantidad en gramos de pan que quiere llevar
+y su tipo.
+Hoy la panaderia tiene una promomción, por cada 3 Kilos de pan, se regala un alfajor.
+(Excepto por compra de pan integral)
+
+Cuando el programa finalice se debe indicar los datos de entrada, salida e indicar 
+si el cliente obtuvo o no de regalo uno o más alfajores
+"""
