d8a32c76bb2aaa57189e5f3ce16a418c360072e6
Christian Sarmiento <christian.sarmiento@gmail.com>
2024-07-30 21:36:27 -0400

    add ciclos2

--- 30-07-2024/ciclos.py
+++ 30-07-2024/ciclos.py
@@ -1,11 +1,46 @@
 """
 Generar un algoritmo que permita ingresar 10 números y mostrar el promedio de los números múltiplos de 3 ingresados
 """
-
-
+"""numeros=0
+suma=0
+for x in range(10):
+    n=int(input("Ingrese un numero "))
+    if n%3==0:
+        numeros+=1
+        suma+=n
+print(suma/numeros)       
+"""     
 """
 Generar un algoritmo que permita ingresar dos números.
 Si el primer número es mayor que el segundo muestra la suma
 Si son iguales muestra la multiplicación
 Si el segundo es mayor que el primero muestra la tabla del 5 de ese número
+"""
+n1 = int(input("Ingrese primer valor"))
+n2 = int(input("Ingrese segundo valor"))
+if n1 > n2:
+    print(n1+n2)
+elif n1 == n2:
+    print(n1*n2)
+else:
+    for x in range(1,11):
+        print(f"{n2} *{5} = {n2*5}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Generar un algoritmo que permita ingresar un número, debe ser mayor a 10.
+En caso contrario escribe 5 veces la palabra error y vuelve a pedir el número.
+El usuario decide cuando terminar.
+
 """
