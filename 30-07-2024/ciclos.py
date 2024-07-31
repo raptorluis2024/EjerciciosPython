299fe1a2a60b5cfe537b17ff36bb1ff567c3cbf5
Christian Sarmiento <christian.sarmiento@gmail.com>
2024-07-30 21:49:18 -0400

    add  ciclos 3

--- 30-07-2024/ciclos.py
+++ 30-07-2024/ciclos.py
@@ -17,6 +17,7 @@ Si son iguales muestra la multiplicación
 Si el segundo es mayor que el primero muestra la tabla de
 multiplicar de ese número
 """
+"""
 n1 = int(input("Ingrese primer valor"))
 n2 = int(input("Ingrese segundo valor"))
 if n1 > n2:
@@ -26,6 +27,7 @@ elif n1 == n2:
 else:
     for x in range(1,11):
         print(f"{n2} *{x} = {n2*x}")
+"""
 
 """
 Generar un algoritmo que permita ingresar un número, debe ser mayor a 10.
@@ -33,3 +35,15 @@ En caso contrario escribe 5 veces la palabra error y vuelve a pedir el número.
 El usuario decide cuando terminar.
 
 """
+
+while True:
+    num=int(input("Ingrese un numero"))
+    
+    if num<=10:
+        for x in range(5):
+            print("ERROR")
+    else:
+        print(f"Numero ingresado: {num} ")
+        op=input("Desea continuar? S/N").upper()
+        if op=='N':
+            break
