ea478de778f98ae27dee7691f15af5b8d9c54f7b
Christian Sarmiento <christian.sarmiento@gmail.com>
2024-08-01 22:16:59 -0400

    add metodos con listas

---
+++ 01-08-2024/ejemplo1.py
@@ -0,0 +1,2 @@
+lista = [{k:v for k,v in zip([1,2],["a","b"])} for x in range(3)]
+print(lista)
---
+++ 02-08-2024/manejodelistas.py
@@ -0,0 +1,10 @@
+lista_de_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9 ,10]
+#print(lista_de_numeros.__dir__())
+#print(lista_de_numeros)
+#lista_de_numeros.reverse()
+#print(lista_de_numeros)
+
+lista_de_numeros.insert(1,"hola")
+print(lista_de_numeros)
+#dic ={}
+#print(dic.__dir__())
