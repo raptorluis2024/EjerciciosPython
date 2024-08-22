abc7b08f2bd2cde5bafb2ba7bbbbf551252adcd9
Luis Peña <raptor_luis@hotmail.com>
2024-08-21 21:05:20 -0400

    add tiempo pizza

--- 21_08_2024/vars.py
+++ 21_08_2024/vars.py
@@ -3,9 +3,10 @@ print(pizza["salsa"] )
 pizza["salsa"] = "pesto"
 print(pizza)
 pizza["ingredientes"].append("champiñones")
+pizza["ingredientes"].append("choclo")
 print(pizza)
 if "choclo" in pizza["ingredientes"]:
     print("ok")
-    
-pizza["ingredientes"].pop(pizza["ingredientes"].index("queso"))
-print(pizza)
+from tiempo import tiempo_pizza
+#pizza["ingredientes"].pop(pizza["ingredientes"].index("queso"))
+print(pizza, tiempo_pizza(pizza))
